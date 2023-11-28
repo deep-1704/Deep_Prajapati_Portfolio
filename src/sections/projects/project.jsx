1c92b935113575cbad725ccddda166a709715ecd
Deep <dp124551634@gmail.com>
2023-11-28 16:22:24 +0530

    Add image carousel in project section

--- src/sections/projects/project.jsx
+++ src/sections/projects/project.jsx
@@ -1,10 +1,28 @@
 import styles from "./styles.module.css";
 import linkSvg from "../../assets/linkSvg.svg";
+import leftBtn from "../../assets/CarouselLeft.svg";
+import rightBtn from "../../assets/CarouselRight.svg";
+import { useState } from "react";
 
 let Project = ({ info }) => {
+    let [itr,setItr] = useState(0);
+    function moveRight(){
+        let carouselSize = info.PImg.length;
+        setItr((itr+1)%carouselSize);
+    }
+    function moveLeft(){
+        let carouselSize = info.PImg.length;
+        if(itr === 0) setItr(carouselSize-1);
+        else setItr(itr-1);
+    }
     return (
         <div className={styles.projectSection}>
-            <div className={styles.projectImage}><img src={info.PImg} alt="Project Glimpse" style={{width:"100%"}}/></div>
+            <div className={styles.projectImageCarousel}>
+                <div className={styles.projectImageCarouselbtn} onClick={e=>moveLeft()}><img src={leftBtn} alt="Left btn" style={{ width: "100%" }} /></div>
+                <div className={styles.projectImage}><img src={info.PImg[itr]} alt="Project Glimpse" style={{ width: "100%" }} /></div>
+                <div className={styles.projectImageCarouselbtn} onClick={e=>moveRight()}><img src={rightBtn} alt="Right btn" style={{ width: "100%" }} /></div>
+            </div>
+
             <div className={styles.projectInfo}>
                 <div className={styles.projectTitle}>{info.PTitle}</div>
                 <div className={styles.projectDescription}>{info.PDescription}</div>
@@ -23,9 +41,9 @@ let Project = ({ info }) => {
                         })}
                     </div>
                 </div>
-                <button className={styles.projectGitLink} onClick={(e)=>window.open(info.gitLink,"_blank")}>
+                <button className={styles.projectGitLink} onClick={(e) => window.open(info.gitLink, "_blank")}>
                     <div>Know more on Github.</div>
-                    <img src={linkSvg} alt="link provided" width="30px"/>
+                    <img src={linkSvg} alt="link provided" width="30px" />
                 </button>
             </div>
         </div>
--- src/sections/projects/projects.jsx
+++ src/sections/projects/projects.jsx
@@ -1,11 +1,22 @@
 import styles from "./styles.module.css";
-import askadocSS from "../../assets/AskaDocSS.png"
+import askadocSS1 from "../../assets/AskaDocSS.png"
+import askadocSS2 from "../../assets/hp2.png"
+import askadocSS3 from "../../assets/Login.png"
+import askadocSS4 from "../../assets/Signup.png"
+import askadocSS5 from "../../assets/Categories1.png"
+import askadocSS6 from "../../assets/Categories2.png"
+import askadocSS7 from "../../assets/DoctorList.png"
+import askadocSS8 from "../../assets/incompleteProfile.png"
+import askadocSS9 from "../../assets/DocProfile.png"
+import askadocSS10 from "../../assets/ChatDtP.png"
+import askadocSS11 from "../../assets/ChatPtD.png"
 import Project from "./project";
 
 let Projects = () => {
+    let projectSS1 = [askadocSS1,askadocSS2,askadocSS3,askadocSS4,askadocSS5,askadocSS6,askadocSS7,askadocSS8,askadocSS9,askadocSS10,askadocSS11]
     let info = [
         {
-            "PImg": askadocSS,
+            "PImg": projectSS1,
             "PTitle" : "AskaDoc - Online Consultancy Web-App",
             "PDescription" : "An online platform connecting patients with a wide range of expert doctors for efficient and accessible medical consultations via chat.",
             "PDetails" : [
