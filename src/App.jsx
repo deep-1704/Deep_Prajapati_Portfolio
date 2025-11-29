61ec1d873608c689ba3405d76e5058773df54c7e
Deep <dp124551634@gmail.com>
2025-11-29 12:29:48 +0530

    Cleanuo for new implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
-import styles from "./appStyles.module.css";
-import Navbar from "./sections/navbar/navbar.jsx";
-import Intro from "./sections/intro/intro";
-import Education from "./sections/education/education.jsx";
-import Achievements from "./sections/achievements/achievements";
-import Skills from "./sections/skills/skills";
-import Projects from "./sections/projects/projects";
-import Contacts from "./sections/contact/contacts";
+// Create a sample app
 let App = () => {
     return (
-        <div className={styles.Container}>
-            <Navbar />
-            <Intro />
-            <Education />
-            <Achievements />
-            <Skills />
-            <Projects />
-            <Contacts />
+        <div>
+            <h1>Deep Prajapati</h1>
         </div>
     );
 }
